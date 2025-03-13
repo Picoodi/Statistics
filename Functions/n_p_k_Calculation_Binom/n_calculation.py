7569afc03f86c0bea4dbf2622ed64f1428a7be11
MaxO <maxi.opel.16@gmail.com>
2025-03-13 19:22:56 +0100

    Adding a functions folder and implementing n and p calculation for the binominal distribution

--- Functions/n_p_k_Calculation_Binom/n_calculation.py
+++ Functions/n_p_k_Calculation_Binom/n_calculation.py
@@ -1,13 +1,11 @@
 #Function for calculating the n of the binomial distribution
 #We need the parameter p and the k and the probability for k
-
+#If we know the value for k = 0
 #But we need the input in the fom 1-P(X = k) =>  probability
 
-#And we have to try to get to the right value with trying
-#Only if k = 0 we can calculate it directly
 
-
-from math import log, ceil
+from math import log, ceil, factorial
+from scipy.stats import binom
 
 
 def n_calculation_binomial(p,k, k_prob):
@@ -15,10 +13,14 @@ def n_calculation_binomial(p,k, k_prob):
     if k == 0:
         n = log(1-k_prob, 1-p)
         return ceil(n)
-
     else:
-        pass #implement here
-
+        n = k  # Minimum trials to have k successes
+        while True:
+            prob = 1 - binom.cdf(k - 1, n, p)
+            if prob >= k_prob:
+                return n
+            n += 1
 
 
 print(n_calculation_binomial(0.234, 0, 0.99)) #tescase k = 0
+print(n_calculation_binomial(0.45, 3, 0.921456))
